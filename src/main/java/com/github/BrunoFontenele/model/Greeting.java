f82678680a4dc50f002159f8eeaf4b17519318a1
Bruno Fontenele <brunomatos2505@gmail.com>
2026-02-08 11:34:07 +0000

    feat: add Model Package and Greeting Record

---
+++ src/main/java/com/github/BrunoFontenele/model/Greeting.java
@@ -0,0 +1,3 @@
+package com.github.BrunoFontenele.model;
+
+public record Greeting(long id, String content) {}
---
+++ src/test/java/com/github/BrunoFontenele/StartupTests.java
@@ -0,0 +1,13 @@
+package com.github.BrunoFontenele;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
